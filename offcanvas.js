944f36ff67f9ba70db56caee9dc3d97adc43d450
shinzz2 <dhafin5858@gmail.com>
2021-12-26 20:57:46 +0700

    dawdadadadadaa

--- offcanvas.js
+++ offcanvas.js
@@ -1,7 +1,14 @@
 (function () {
-  'use strict'
+  "use strict";
 
-  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
-    document.querySelector('.offcanvas-collapse').classList.toggle('open')
-  })
-})()
+  document.querySelector("#navbarSideCollapse").addEventListener("click", function () {
+    document.querySelector(".offcanvas-collapse").classList.toggle("open");
+  });
+})();
+
+var myModal = document.getElementById("myModal");
+var myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", function () {
+  myInput.focus();
+});
