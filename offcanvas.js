471724adc70b56876c500cd11b2225115d1656ed
shinzz2 <dhafin5858@gmail.com>
2021-12-26 21:12:06 +0700

    adawda

--- offcanvas.js
+++ offcanvas.js
@@ -12,3 +12,5 @@ var myInput = document.getElementById("myInput");
 myModal.addEventListener("shown.bs.modal", function () {
   myInput.focus();
 });
+
+<script type="module">import {Toast} from 'bootstrap.esm.min.js' Array.from(document.querySelectorAll('.toast')) .forEach(toastNode => new Toast(toastNode))</script>;
