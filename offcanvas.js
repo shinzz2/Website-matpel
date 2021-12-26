a155eef816e39f0dee0ab12ab733242974246725
shinzz2 <dhafin5858@gmail.com>
2021-12-27 00:13:49 +0700

    dawadawd

--- offcanvas.js
+++ offcanvas.js
@@ -5,3 +5,10 @@
     document.querySelector(".offcanvas-collapse").classList.toggle("open");
   });
 })();
+
+var myModal = document.getElementById("myModal");
+var myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", function () {
+  myInput.focus();
+});
